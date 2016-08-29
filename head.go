64d07da4191334a72f1557c20d02bcbb3ce416b1
Ernestas Poskus <ernestas.poskus@gmail.com>
2016-08-29 21:43:25 +0300

    Add function JavascriptTag

--- head.go
+++ head.go
@@ -8,6 +8,7 @@ import (
 // -
 var (
 	StylesheetTagPath = "/assets/stylesheets"
+	JavascriptTagPath = "/assets/javascripts"
 )
 
 // StylesheetTag - generates StylesheetTag HEAD element
@@ -16,6 +17,16 @@ func StylesheetTag(href string) template.HTML {
 		fmt.Sprintf(`<link type="text/css" rel="stylesheet" href="%s/%s">`,
 			StylesheetTagPath,
 			href,
+		),
+	)
+}
+
+// JavascriptTag - generates <script> element
+func JavascriptTag(src string) template.HTML {
+	return template.HTML(
+		fmt.Sprintf(`<script src="%s/%s" async></script>`,
+			JavascriptTagPath,
+			src,
 		),
 	)
 }
--- head_test.go
+++ head_test.go
@@ -11,3 +11,8 @@ func TestStylesheetTag(t *testing.T) {
 	expected := template.HTML(`<link type="text/css" rel="stylesheet" href="/assets/stylesheets/test.css">`)
 	assert.Equal(t, expected, StylesheetTag("test.css"))
 }
+
+func TestJavascriptTag(t *testing.T) {
+	expected := template.HTML(`<script src="/assets/javascripts/app.js" async></script>`)
+	assert.Equal(t, expected, JavascriptTag("app.js"))
+}
